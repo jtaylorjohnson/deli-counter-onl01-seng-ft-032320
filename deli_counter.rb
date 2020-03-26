d617ef33b5f45e6f79a549617c4019a0953be0b1
bustling-clone-9138 <jtaylorjohnson@gmail.com>
2020-03-26 01:27:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.count == 0 
+    puts "The line is currently empty."
+  else 
+    katz_deli.each_with_index do |person, number|
+    puts "The line is currently: #{number+1}. #{person}"
+    end
+  end
+end
+
+def take_a_number(katz_deli, name)
+  puts "Welcome, #{name}. You are number #{katz_deli.count + 1} in line."
+end
+
+def now_serving
+  
+end
+
+new_array = katz_deli.map do |here|
+  "#{here}"
+end
