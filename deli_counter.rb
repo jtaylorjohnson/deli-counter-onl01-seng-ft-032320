4afe8f8de181b9e7c621ab3b03a47e86676a129c
bustling-clone-9138 <jtaylorjohnson@gmail.com>
2020-03-26 02:33:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -28,7 +28,8 @@ def now_serving(katz_deli)
   if katz_deli.count == 0 
     puts "There is nobody waiting to be served!"
   else
-  puts "Currently serving #{katz_deli[0]}."
+  puts "Currently serving #{katz_deli.first}."
+  
   end 
 end
 
